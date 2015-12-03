524110e799e2bc1c32c6879e2e67553705322ded
JCook <nyorksjc@hotmail.com>
2015-12-03 10:14:01 -0500

    Added checkout and purchase functionality

--- app/src/main/java/com/example/android/dinnerapp/OrderDinnerActivity.java
+++ app/src/main/java/com/example/android/dinnerapp/OrderDinnerActivity.java
@@ -18,20 +18,33 @@ package com.example.android.dinnerapp;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+import com.google.android.gms.analytics.ecommerce.Product;
+import com.google.android.gms.analytics.ecommerce.ProductAction;
 
 
 public class OrderDinnerActivity extends Activity {
     String selectedDinnerExtrasKey = String.valueOf(R.string.selected_dinner);
+    String thisDinner;
+    Button btnAddToCart;
+    Button btnStartCheckout;
+    Button btnPurchase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.show_info);
+        setContentView(R.layout.order_dinner);
     }
 
     protected void onStart() {
         super.onStart();
+
+
 
         // Set the heading
         TextView heading_tv = (TextView) findViewById(R.id.textView_info_heading);
@@ -39,10 +52,143 @@ public class OrderDinnerActivity extends Activity {
 
         // Set the text
         TextView tv = (TextView) findViewById(R.id.textView_info);
+        btnAddToCart = (Button) findViewById(R.id.add_to_cart_btn);
+        btnStartCheckout = (Button) findViewById(R.id.start_checkout);
+        btnPurchase = (Button) findViewById(R.id.btnPurchase);
 
-        String dinner = getIntent().getStringExtra(selectedDinnerExtrasKey);
+        thisDinner = getIntent().getStringExtra(selectedDinnerExtrasKey);
         tv.setText("This is where you will order the selected dinner: \n\n" +
-                dinner);
+                thisDinner);
+
+        sendViewProductHit(thisDinner, Utility.getDinnerId(thisDinner));
+    }
+
+    public void sendViewProductHit(String dinner, String dinnerID) {
+
+        Product product = new Product()
+                .setName("dinner")
+                .setPrice(5)
+                .setVariant(dinner)
+                .setId(dinnerID)
+                .setQuantity(1);
+
+        ProductAction productAction = new ProductAction(ProductAction.ACTION_DETAIL);
+
+        Tracker tracker = ((MyApplication) getApplication()).getTracker();
+
+        tracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Shopping steps")
+                .setAction("View Order Dinner Screen")
+                .setLabel(dinner)
+                .addProduct(product)
+                .setProductAction(productAction)
+                .build());
+    }
+
+    public void addDinnerToCart(View view) {
+
+        // Code goes here to add the dinner to the cart
+
+        Utility.showMyToast("I will add the dinner " + thisDinner + " to the cart", this);
+
+        // Also send an Analytics hit
+        sendAddToCartHit();
+
+        btnAddToCart.setVisibility(View.INVISIBLE);
+        btnStartCheckout.setVisibility(View.VISIBLE);
+
+
+    }
+
+    private  void sendAddToCartHit() {
+
+        Product product = new Product()
+                .setName("dinner")
+                .setPrice(5)
+                .setVariant(thisDinner)
+                .setId(Utility.getDinnerId(thisDinner))
+                .setQuantity(1);
+
+        ProductAction productAction = new ProductAction(ProductAction.ACTION_ADD);
+
+        Tracker tracker = ((MyApplication) getApplication()).getTracker();
+
+        tracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Shopping steps")
+                .setAction("Add Order To Cart")
+                .setLabel(thisDinner)
+                .addProduct(product)
+                .setProductAction(productAction)
+                .build());
+
+    }
+
+    public void startCheckout(View view) {
+
+        // Code goes here to add the dinner to the cart
+
+        Utility.showMyToast("Starting the checkout process for " + thisDinner, this);
+
+        // Also send an Analytics hit
+        sendStartCheckoutHit();
+        btnPurchase.setVisibility(View.VISIBLE);
+        btnStartCheckout.setVisibility(View.INVISIBLE);
+    }
+
+    private  void sendStartCheckoutHit() {
+
+        Product product = new Product()
+                .setName("dinner")
+                .setPrice(5)
+                .setVariant(thisDinner)
+                .setId(Utility.getDinnerId(thisDinner))
+                .setQuantity(1);
+
+        ProductAction productAction = new ProductAction(ProductAction.ACTION_CHECKOUT);
+
+        Tracker tracker = ((MyApplication) getApplication()).getTracker();
+
+        tracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Shopping steps")
+                .setAction("Start checkout")
+                .setLabel(thisDinner)
+                .addProduct(product)
+                .setProductAction(productAction)
+                .build());
+
+    }
+
+    public void purchaseDinner(View view) {
+
+        // Code goes here to add the dinner to the cart
+
+        Utility.showMyToast("Purchasing " + thisDinner, this);
+
+        // Also send an Analytics hit
+        sendPurchaseHit();
+    }
+
+    private  void sendPurchaseHit() {
+
+        Product product = new Product()
+                .setName("dinner")
+                .setPrice(5)
+                .setVariant(thisDinner)
+                .setId(Utility.getDinnerId(thisDinner))
+                .setQuantity(1);
+
+        ProductAction productAction = new ProductAction(ProductAction.ACTION_PURCHASE).setTransactionId(Utility.getUniqueTransactionId(Utility.getDinnerId(thisDinner)));
+
+        Tracker tracker = ((MyApplication) getApplication()).getTracker();
+
+        tracker.send(new HitBuilders.EventBuilder()
+                .setCategory("Shopping steps")
+                .setAction("Purchase")
+                .setLabel(thisDinner)
+                .addProduct(product)
+                .setProductAction(productAction)
+                .build());
+
     }
 
 }
--- app/src/main/java/com/example/android/dinnerapp/Utility.java
+++ app/src/main/java/com/example/android/dinnerapp/Utility.java
@@ -19,6 +19,9 @@ package com.example.android.dinnerapp;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by jocelyn on 12/10/14.
  */
@@ -46,5 +49,19 @@ public class Utility {
 
     public static String[] combine(String[] a, String[] b, String[] c, String[] d){
         return combine(combine(a, b), combine(c, d));
+    }
+
+    public static String getDinnerId(String dinner) {
+        return dinner.substring(0, 2);
+    }
+
+    public static String getCurrentTime() {
+        Date curDate = new Date();
+        SimpleDateFormat format = new SimpleDateFormat("dd-M hh:mm:ss");
+        return format.format(curDate);
+    }
+
+    public static String getUniqueTransactionId (String productId) {
+        return ("T-" + getCurrentTime() + productId);
     }
 }
